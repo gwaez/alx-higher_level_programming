d26d8185cc617416b94ec4c1e68fffea8a554b83
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2023-12-04 12:11:30 -0800

    gwz

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Ahmed Mahmoud (GitHub: [gwaez])
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
