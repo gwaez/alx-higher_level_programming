98df5c4cb3db925aedfac1ccb1a1cbe07c135ffc
Ahmed Mahmoud <eng.ahmed18@gmail.com>
2024-05-13 15:17:41 +0000

    gw@3z

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records of the table second_table, ordered by score (top first) and displaying both the score and the name.
+
+SELECT score, name FROM second_table ORDER BY score DESC;
+
